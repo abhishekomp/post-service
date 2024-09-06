9c8e4a12592d38132364c6e89a257c79da20a595
abhishekomp <abhishek2283@hotmail.com>
2024-09-06 22:04:52 +0200

    updated property for test to run successfully as the data loaded to db is not same with json and post objects.

--- src/test/java/org/aom/post_service/PostServiceApplicationIntegrationTests.java
+++ src/test/java/org/aom/post_service/PostServiceApplicationIntegrationTests.java
@@ -1,6 +1,5 @@
 package org.aom.post_service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
